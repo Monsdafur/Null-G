ec46fd6143dfcad6ad09ac8bfb59b156ee5413ec
Hoa <dqhoa06@gmail.com>
2026-06-24 19:33:51 +0700

    improved laser emitter logic

--- scripts/ray_emitter.gd
+++ scripts/ray_emitter.gd
@@ -9,77 +9,57 @@ enum Direction {
 }
 
 @export var direction: Direction = Direction.RIGHT
-@export var length: int = 30
 @onready var sprite_frames: SpriteFrames = preload("res://assets/animation/ray.tres")
 @onready var ray: RayCast2D = $RayCast2D
 @onready var trigger: Area2D = $Trigger
+@onready var segment: AnimatedSprite2D = $Segment
+@onready var hit: AnimatedSprite2D = $Hit
 
-var sprites: Array[AnimatedSprite2D]
 var direction_vector: Vector2
-var pre_length: int
-var segment_animation: String
-var hit_animation: String
-
-func switch_animation(index: int, animation: String) -> void:
-	var frame: int = sprites[index].frame
-	var progress: float = sprites[index].frame_progress
-	sprites[index].play(animation)
-	sprites[index].frame = frame
-	sprites[index].frame_progress = progress
+var pre_length: float = 0.0
 
 func _ready() -> void:
 	trigger.get_node("CollisionShape2D").shape = RectangleShape2D.new()
+	trigger.get_node("CollisionShape2D").shape.size = Vector2(16.0, 16.0)
 	match direction:
 			Direction.LEFT:
 				direction_vector = Vector2(-1.0, 0.0)
-				segment_animation = "horizontal_segment"
-				hit_animation = "left_hit"
+				segment.play("horizontal_segment")
+				hit.play("left_hit");
 			Direction.RIGHT:
 				direction_vector = Vector2(1.0, 0.0)
-				segment_animation = "horizontal_segment"
-				hit_animation = "right_hit"
+				segment.play("horizontal_segment")
+				hit.play("right_hit");
 			Direction.UP:
 				direction_vector = Vector2(0.0, -1.0)
-				segment_animation = "vertical_segment"
-				hit_animation = "up_hit"
+				segment.play("vertical_segment")
+				hit.play("up_hit");
 			Direction.DOWN:
 				direction_vector = Vector2(0.0, 1.0)
-				segment_animation = "vertical_segment"
-				hit_animation = "down_hit"
-				
-	for i: int in range(length):
-		var sprite: AnimatedSprite2D = AnimatedSprite2D.new()
-		sprite.position = direction_vector * float(i + 1) * 16.0
-		sprite.sprite_frames = sprite_frames
-		sprite.visible = false
-		sprite.z_index = -1
-		add_child(sprite)
-		sprites.append(sprite)
-		sprite.play(segment_animation)
-			
-	ray.target_position = sprites.back().position + direction_vector * 8.0
+				segment.play("vertical_segment")
+				hit.play("down_hit");
+	ray.target_position = ray.position + direction_vector * 30.0 * 16.0
+	ray.position += direction_vector * 8.0
 
 func _process(_delta: float) -> void:
 	if not ray.is_colliding():
 		return
 	
-	var true_length = (ray.get_collision_point() - global_position).length()
-	var current_length = int(floor(true_length / 16.0))
-	if (current_length == pre_length):
+	var point: Vector2 = ray.get_collision_point()
+	var length = (point - ray.global_position).length()
+	if abs(length - pre_length) < 0.005:
 		return
-	pre_length = current_length
 	
-	var offset = direction_vector * true_length * 0.5
+	var offset = ray.position + direction_vector * length * 0.5
 	trigger.position = offset
+	segment.position = offset
+	hit.global_position = point - direction_vector * 8.0
 	if direction == Direction.UP or direction == Direction.DOWN:
-		trigger.get_node("CollisionShape2D").shape.size = Vector2(4.0, true_length)
+		trigger.get_node("CollisionShape2D").shape.size = Vector2(4.0, length)
+		segment.scale = Vector2(1.0, length / 16.0)
 	elif direction == Direction.RIGHT or direction == Direction.LEFT:
-		trigger.get_node("CollisionShape2D").shape.size = Vector2(true_length, 4.0)
-	
-	for i: int in range(length):
-		switch_animation(i, segment_animation)
-		sprites[i].visible = i <= current_length
-	switch_animation(current_length - 1, hit_animation)
+		trigger.get_node("CollisionShape2D").shape.size = Vector2(length, 4.0)
+		segment.scale = Vector2(length / 16.0, 1.0)
 
 func _on_trigger_body_entered(_body: Node2D) -> void:
 	game_manager.game_over.emit()
