6ca9c00a9cc36fd500c8f9d2236e562cb43d526a
Hoa <dqhoa06@gmail.com>
2026-06-28 08:02:43 +0700

    changed art assets

--- scripts/projector.gd
+++ scripts/projector.gd
@@ -18,8 +18,8 @@ enum Type {
 
 func _ready() -> void:
 	sprite.region_rect = Rect2i(64, 128, 16, 16) if not reversed else Rect2i(80, 128, 16, 16)
-	collider.position = Vector2(0.0, 5.5) if not reversed else Vector2(0.0, -5.5)
-	trigger.position = Vector2(0.0, 2.0) if not reversed else Vector2(0.0, -2.0)
+	collider.position = Vector2(0.0, 3.5) if not reversed else Vector2(0.0, -3.5)
+	trigger.position = Vector2(0.0, -2.0) if not reversed else Vector2(0.0, 2.0)
 
 func _on_trigger_body_entered(_body: Node2D) -> void:
 	if (global.gravity_scale < 0.0 and reversed) or (global.gravity_scale > 0.0 and not reversed):
